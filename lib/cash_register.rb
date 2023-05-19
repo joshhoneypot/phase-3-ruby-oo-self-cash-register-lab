3011a898f7946511389313ab542419e9f61ddcd6
joshhoneypot <joshuaangugo@gmail.com>
2023-05-19 08:59:42 +0300

    Half baked

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+class CashRegister
+    attr_reader :discount, :total, :title, :price
+    
+    def initialize (employee_discount=0, total=0)
+        @discount = employee_discount
+        @total=total
+    end
 
+    def total=(total)
+        @total=total
+    end
+
+    def add_item(title, price, quantity = 1)
+        item_total = price * quantity
+        @total += item_total
+    end
+
+    def apply_discount
+        if @discount > 0
+          discount_amount = @total * (@discount.to_f / 100)
+          @total -= discount_amount
+          "After the discount, the total comes to $#{'%.0f' % @total}."
+        else
+          "There is no discount to apply."
+        end
+    end
+end
