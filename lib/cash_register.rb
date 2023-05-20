fea089eba4b183d94acf8d61f2c13c2a0648220c
joshhoneypot <joshuaangugo@gmail.com>
2023-05-20 06:18:47 +0300

    Last method

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,27 +1,36 @@
 class CashRegister
-    attr_reader :discount, :total, :title, :price
-    
-    def initialize (employee_discount=0, total=0)
-        @discount = employee_discount
-        @total=total
+    attr_reader :discount, :total, :items
+  
+    def initialize(employee_discount = 0, total = 0.0)
+      @discount = employee_discount
+      @total = total
+      @items = []
     end
-
+  
     def total=(total)
-        @total=total
+      @total = total
     end
-
+  
     def add_item(title, price, quantity = 1)
-        item_total = price * quantity
-        @total += item_total
+      item_total = price * quantity
+      @total += item_total
+      quantity.times { @items << title }
     end
-
+  
     def apply_discount
-        if @discount > 0
-          discount_amount = @total * (@discount.to_f / 100)
-          @total -= discount_amount
-          "After the discount, the total comes to $#{'%.0f' % @total}."
-        else
-          "There is no discount to apply."
-        end
+      if @discount > 0
+        discount_amount = @total * (@discount.to_f / 100)
+        @total -= discount_amount
+        "After the discount, the total comes to $#{'%.0f' % @total}."
+      else
+        "There is no discount to apply."
+      end
     end
-end
+    
+    def void_last_transaction
+        last_item_price = @items.pop
+        @total -= last_item_price
+        @total = 0.0 if @items.empty?
+    end  
+  end
+  
